26fa60586792a4f5b720eb2582cff3203cc0dac0
土豆大王 <1551310903@qq.com>
2026-07-03 14:55:17 +0800

    切换为手机版 web-mobile 构建

--- docs/application.89bbc.js
+++ docs/application.89bbc.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.660f8.json';
+          this.settingsPath = 'src/settings.eee8c.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
